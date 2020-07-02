Bias backprop issue solved (Derivatives have been corrected) Currently there is a relu exploding issue

network.py
@@ -17,8 +17,6 @@ factor = 0.99/255
 x_train = ((x_train - np.mean(x_train))/ np.std(x_train))/255 
 
 
-print(y_train[0:20])
-
 x_train = x_train.reshape(60000,784).T
 
 
@@ -33,15 +31,14 @@ class network:
 		self.cache = {}
 		self.cache['x'] = x
 		self.cache['y'] = y
-		self.cache['w1'] = np.random.random((784,100))
-		self.cache['w2'] = np.random.random((100,72))
+		self.cache['w1'] = np.random.random((784,84))
+		self.cache['w2'] = np.random.random((84,72))
 		self.cache['w3'] = np.random.random((72,54))
 		self.cache['w4'] = np.random.random((54,10))
-		self.cache['b1'] = np.random.random((100,1))
+		self.cache['b1'] = np.random.random((84,1))
 		self.cache['b2'] = np.random.random((72,1))
 		self.cache['b3'] = np.random.random((54,1))
 		self.cache['b4'] = np.random.random((10,1))
-		
 		with open("accuracy.csv" , "w") as f:
 			t = csv.DictWriter(f , fieldnames = ['cost' , "accuracy"])
 			t.writeheader()
@@ -53,26 +50,26 @@ class network:
 		if x is None:
 			x = self.cache['x']
 		w1,w2,w3,w4,b1,b2,b3,b4 = self.get('w1','w2','w3','w4','b1','b2','b3','b4')
-		z1 = np.dot(w1.T,x ) +b1
-
+		
+		z1 = np.dot(w1.T,x) + b1
 		a1 = relu(z1)
 		dz1 = relu_prime(z1)
 
-	
 		z2 = np.dot(w2.T,a1) + b2
-	
-
 		a2 = relu(z2)
 		dz2 = relu_prime(z2)
 
-
-
-		z3 = np.dot(w3.T ,a2) +b3
+		z3 = np.dot(w3.T,a2) + b3
 		a3 = relu(z3)
 		dz3 = relu_prime(z3)
 
 		z4 = np.dot(w4.T,a3) + b4
 		a4 = softmax(z4)
+
+		print(a4)
+		exit()
+
+		#print(b1.shape,b2.shape,b3.shape,b4.shape)
 
 		self.put(dz1=dz1,dz2=dz2,dz3=dz3,a4=a4,a3=a3,a2=a2,a1=a1)
 		return a4
@@ -119,29 +116,41 @@ class network:
 			x = self.cache['x']
 
 		a4,a3,a2,a1,w4,w3,w2,w1,dz3,dz2,dz1= self.get('a4','a3','a2','a1','w4','w3','w2','w1','dz3','dz2','dz1')
-
+		
 		theta4 = (a4 - y.T)
 		theta3 = np.multiply(dz3,np.dot(w4,theta4))
 		theta2 = np.multiply(dz2,np.dot(w3,theta3))
 		theta1 = np.multiply(dz1,np.dot(w2,theta2))
+
+		db4 =  np.sum(theta4 , axis = 1 , keepdims = True)/32
+		db3 =  np.sum(dz3 , axis = 1 , keepdims = True)/32
+		db2 =  np.sum(dz2 , axis = 1 , keepdims = True)/32
+		db1 =  np.sum(dz1 , axis = 1 , keepdims = True)/32
+
 
 		dw4 = np.dot(theta4,a3.T)/32
 		dw3 = np.dot(theta3,a2.T)/32
 		dw2 = np.dot(theta2,a1.T)/32
 		dw1 = np.dot(theta1,x.T)/32
 
+		self.put(db4=db4,db3=db3,db2=db2,db1=db1)
+		self.put(dw4=dw4,dw2=dw2,dw3=dw3,dw1=dw1)
 
-		self.put(dw4 = dw4,dw2=dw2,dw3=dw3,dw1=dw1)
-
-	def update(self,rate = 0.0005):
+	def update(self,rate = 0.05):
 		w1,w2,w3,w4,dw1,dw2,dw3,dw4 = self.get('w1','w2','w3','w4','dw1','dw2','dw3','dw4')
+		b1,b2,b3,b4,db1,db2,db3,db4 = self.get('b1','b2','b3','b4','db1','db2','db3','db4')
 
 		w1 -= rate*dw1.T
 		w2 -= rate*dw2.T
 		w3 -= rate*dw3.T
 		w4 -= rate*dw4.T
+		
+		b1 -= rate*db1
+		b2 -= rate*db2
+		b3 -= rate*db3
+		b4 -= rate*db4
 
-
+		self.put(b1=b1,b2=b2,b3=b3,b4=b4)
 		self.put(w1=w1,w2=w2,w3=w3,w4=w4)
 
 
@@ -157,9 +166,9 @@ class network:
 def main():
 	net = network(x_train,y_train)
 
-	costs = net.train(epoch = 200)
+	costs = net.train(epoch = 50)
 
-	costs = [cost for i,cost in enumerate(costs) if i%1000 == 0]
+	costs = [cost for i,cost in enumerate(costs) if i%250 == 0]
 	
 	pred = np.argmax((net.forward(x_test).T), axis = 1).reshape(10000,1)
 	y = np.argmax(y_test , axis = 1).T.reshape(10000,1)